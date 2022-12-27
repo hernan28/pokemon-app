e257d6bad69af7d5d311f34e290a9321d6c46221
AllanCSR09 <allancs.electro@gmail.com>
2022-12-26 22:23:38 -0600

    Fixes to add generic list

--- src/component/Stats/PokemonStats.js
+++ src/component/Stats/PokemonStats.js
@@ -1,49 +1,38 @@
 import React from "react";
 import StatBar from "./StatBar";
 
-//The idea is to use two lists(Statname,Statvalue) as inputs of PokemonStats component. I'm using raw values by the moment
+const Row = ({ statName, statValue }) => (
+  <tr>
+    <td align="right">{statName}</td>
+    <td
+      style={{
+        paddingLeft: "20px",
+        paddingRight: "20px",
+        color: "black",
+        fontWeight: "lighter",
+      }}
+    >
+      {statValue}
+    </td>
+    <td width={180}>
+      <StatBar value={statValue} />
+    </td>
+  </tr>
+);
 
-const PokemonStats = () => {
-    return (
-        <table 
-            style={{
-                color: "gray",
-                fontFamily: "Rubik,sans-serif",
-                fontWeight: "bold",
-                fontSize: "13px",
-            }} cellPadding ="3px" >
-            <tr>
-                <td align="right" >Hp</td>
-                <td style={{paddingLeft:"20px",paddingRight:"20px", color:"black", fontWeight:"lighter"}}>45</td>
-                <td width={180}><StatBar value={45} /></td>
-            </tr>
-            <tr>
-                <td align="right">Attack</td>
-                <td style={{paddingLeft:"20px",paddingRight:"20px", color:"black", fontWeight:"lighter"}}>49</td>
-                <td><StatBar value={49} /></td>
-            </tr>
-            <tr>
-                <td align="right">Defense</td>
-                <td style={{paddingLeft:"20px",paddingRight:"20px", color:"black", fontWeight:"lighter"}}>49</td>
-                <td><StatBar value={49} /></td>
-            </tr>
-            <tr>
-                <td align="right">Special Attack</td>
-                <td style={{paddingLeft:"20px",paddingRight:"20px", color:"black", fontWeight:"lighter"}}>65</td>
-                <td><StatBar value={65} /></td>
-            </tr>
-            <tr>
-                <td align="right">Special Defense</td>
-                <td style={{paddingLeft:"20px",paddingRight:"20px", color:"black", fontWeight:"lighter"}}>65</td>
-                <td><StatBar value={65} /></td>
-            </tr>
-            <tr>
-                <td align="right">Speed</td>
-                <td style={{paddingLeft:"20px",paddingRight:"20px", color:"black", fontWeight:"lighter"}}>45</td>
-                <td><StatBar value={45} /></td>
-            </tr>
-        </table>
-    );
-}
-
-export default PokemonStats
+const PokemonStats = ({ statsList }) => (
+  <table
+    style={{
+      color: "gray",
+      fontFamily: "Rubik,sans-serif",
+      fontWeight: "bold",
+      fontSize: "13px",
+    }}
+    cellPadding="3px"
+  >
+    {statsList.map((stat) => (
+      <Row statName={stat.name} statValue={stat.value} />
+    ))}
+  </table>
+);
+export default PokemonStats;
--- src/component/Stats/__stories__/PokemonStats.stories.js
+++ src/component/Stats/__stories__/PokemonStats.stories.js
@@ -1,12 +1,23 @@
 import React from "react";
 import PokemonStats from "../PokemonStats";
 
-export default {
-    title: "Stats/PokemonStats",
-    component: PokemonStats,
-    parameters: {
-      layout: "fullscreen",
-    },
-  };
+const testStatsList = [
+  { name: "Hp", value: 45 },
+  { name: "Attack", value: 49 },
+  { name: "Defense", value: 49 },
+  { name: "Special Attack", value: 65 },
+  { name: "Special Defense", value: 65 },
+  { name: "Speed", value: 45 },
+];
 
-  export const TestPokemonStats = () => <PokemonStats/>;
+export default {
+  title: "Stats/PokemonStats",
+  component: PokemonStats,
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+export const TestPokemonStats = () => (
+  <PokemonStats statsList={testStatsList} />
+);

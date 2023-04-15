d67cd5bd7091c6ce18a2a5168df13d0fd3314b0f
hernan28 <hernanmadrigal28@gmail.com>
2023-04-14 23:33:38 -0600

    Adding new Skeleton, fixing some issues and code refactor to style components

--- src/component/PokemonList.js
+++ src/component/PokemonList.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
+import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
 import useFetch from "react-fetch-hook";
 import InfoCard from "./cards/InfoCard";
 import PokemonStats from "./Stats/PokemonStats";
 
-const PokemonItem = ({ pokemonItem }) => {
+const PokemonItem = ({ pokemonItem, pokeIndex }) => {
   const [runGetData, setRunGetData] = useState(false);
   const { data: { stats = [], ...restPokemonData } = {} } = useFetch(
     pokemonItem.url,
@@ -25,10 +27,30 @@ const PokemonItem = ({ pokemonItem }) => {
         display: "flex",
         alignItems: "center",
         justifyContent: "space-evenly",
+        padding: "30px 20px 20px",
+        backgroundColor: pokeIndex % 2 === 1 ? "#f8f7f7" : undefined,
       }}
     >
-      <InfoCard pokemonData={restPokemonData} />
-      <PokemonStats statsList={pokemonStats} />
+      {Object.keys(restPokemonData).length !== 0 ? (
+        <>
+          <InfoCard pokemonData={restPokemonData} />
+          <PokemonStats statsList={pokemonStats} />
+        </>
+      ) : (
+        <>
+          <Stack spacing={1}>
+            <Skeleton variant="rectangular" width={300} height={200} />
+          </Stack>
+          <Stack spacing={1}>
+            <Skeleton width={300} />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+          </Stack>
+        </>
+      )}
     </div>
   );
 };
@@ -37,10 +59,11 @@ const PokemonList = ({ pokemonList }) => {
   return (
     <div>
       {pokemonList &&
-        pokemonList.map((pokemonItem) => (
+        pokemonList.map((pokemonItem, index) => (
           <PokemonItem
             key={pokemonItem.name ?? pokemonItem.pokemon.name}
             pokemonItem={pokemonItem.name ? pokemonItem : pokemonItem.pokemon}
+            pokeIndex={index}
           />
         ))}
     </div>
